0df93e67b074a9b1719e141d70095e49e97ec344
Sia <siakaramalegos@gmail.com>
2015-03-04 16:16:37 -0600

    Add logical menu and layouts for devise

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
 
   # GET /categories
   # GET /categories.json
--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,6 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
 
   # GET /resources
   # GET /resources.json
