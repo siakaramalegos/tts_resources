fa5c66b091427548fad8a7effc8b27ab8ef6c051
Gant <GantMan@gmail.com>
2015-03-17 10:49:57 -0500

    adds annotations and annotation automated rake task

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
 class Category < ActiveRecord::Base
   has_many :resources
 end
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,16 @@
 class Resource < ActiveRecord::Base
   belongs_to :category
 end
+
+# == Schema Information
+#
+# Table name: resources
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  link        :text
+#  notes       :text
+#  category_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
