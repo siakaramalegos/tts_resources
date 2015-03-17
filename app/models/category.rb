6b67df843b69c1867cc0b125b761fe9ed62e8155
cocoswank <patchkatie@gmail.com>
2015-03-17 12:19:41 -0500

    Add validations to category and resource models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :resources
+
+  validates :name, presence: true, length: {in: 2..25}, uniqueness: true
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,7 @@
 class Resource < ActiveRecord::Base
   belongs_to :category
+
+  validates :title, presence: true, length: {in: 2..40}, uniqueness: true
+  validates :link, presence: true, length: {minimum: 4}, uniqueness: true
+  validates :notes, presence: true, length: {in: 3..200}
 end
