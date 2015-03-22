a538688e86c861640a801d343bf08c8ea42456ea
Sia <siakaramalegos@gmail.com>
2015-03-22 15:48:41 -0500

    Added active admin model and other supporting stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   devise_for :users
   resources :resources
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,6 +23,7 @@ end
 #  last_sign_in_ip        :inet
 #  created_at             :datetime
 #  updated_at             :datetime
+#  admin                  :boolean
 #
 # Indexes
 #
