26a864a1d16ed88f51cdbce40a264b9224fe2895
anne <richardson.ae@gmail.com>
2015-11-03 17:11:40 -0600

    added seeds for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+## schema
+  # create_table "categories"
+    # t.string   "name"
+    # t.text     "description"
+
+Category.create!([
+  {
+    name: "Environment",
+    description: "Cool tools for command line and/or setting up Windows and OSX environments."
+  },
+  {
+    name: "Frameworks",
+    description: "Front-end frameworks"
+  },
+  {
+    name: "Git and Github",
+    description: "Version control tools and learning links"
+  },
+  {
+    name: "Graphics and Color",
+    description: "Front-end design resources like color scheme designers, stock photos, layout design, etc."
+  },
+  {
+    name: "Hosting",
+    description: "App/webpage hosting sites"
+  },
+  {
+    name: "HTML and CSS",
+    description: "References and learning tools for HTML and CSS"
+  },
+  {
+    name: "JavaScript and JQuery",
+    description: "Tools and links for helping with JavaScript , JQuery, and related topics"
+  },
+  {
+    name: "JSON and XML tools",
+    description: "Tools for help with JSON and XML that are not Ruby gems"
+  },
+  {
+    name: "Ruby and Rails Tools",
+    description: "Not gems but other tools that are great to use with Ruby and/or Rails"
+  },
+  {
+    name: "Ruby Gems",
+    description: "Great gems for use in Ruby and Rails"
+  }
+])
