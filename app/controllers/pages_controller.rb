7ac5e5794982193f4f2ae60d47cb3ea19f852236
Sia <siakaramalegos@gmail.com>
2015-11-11 17:59:23 -0600

    added newest and hottest resources to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @newest_resources = Resource.recently_created
+    @hot_resources = Resource.all.sort_by(&:netvotes).reverse.first(5)
   end
 
   def about
